ed8f0e2ec34d4b779196724e0731bc3e8f838212
Pritam roy <pritamroy13052003@gmail.com>
2025-06-08 10:15:40 +0530

    Next.js Setup and routing done

---
+++ app/companions/new/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Newcompanion = () => {
+  return (
+    <div>Newcompanion</div>
+  )
+}
+
+export default Newcompanion
---
+++ app/companions/page.tsx
@@ -0,0 +1,6 @@
+export const CompanionsLibrary = () => {
+  return (
+    <div>pageu1</div>
+  )
+}
+export default CompanionsLibrary 
